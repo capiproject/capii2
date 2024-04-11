b105c875b846067fc4689526582eed67e23d5f31
capiproject <165906396+capiproject@users.noreply.github.com>
2024-04-11 12:07:22 -0300

    pop up

---
+++ capii/Assets/Codes/IrPop.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader5 : MonoBehaviour
+{
+    public void LoadScene5()
+    {
+        SceneManager.LoadScene("NumerosPopUp");
+    }
+}
