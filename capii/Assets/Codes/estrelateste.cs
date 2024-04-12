38e8e98a485e8ab265a35dde37850dc66bf6d54b
capiproject <165906396+capiproject@users.noreply.github.com>
2024-04-12 12:15:18 -0300

    muita coisa

---
+++ capii/Assets/Codes/estrelateste.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes5 : MonoBehaviour
+{
+    public void GoToEstrela()
+    {
+        SceneManager.LoadScene("Atividades");
+    }
+}
---
+++ capii/Assets/Example/Scripts/BleInterador.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Android.BLE;
+using Android.BLE.Commands;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using System.Text;
+using TMPro;
+using System;
+
+public class BleInterador : MonoBehaviour
+{
+    [SerializeField]
+    private Button botaoConectar, botaoDesconectar;
+    [SerializeField]
+    TextMeshProUGUI status;
+    [SerializeField]
+    string nomeDispositivo;
+    [SerializeField]
+    private string _servico = "ffe0", _caracteristica = "ffe1";
+
+
+    //aqui
+    [SerializeField]
+    private int _scanTime = 10;
+
+
+
+
+    private ConnectToDevice _connectCommand;
+
+
+
+    IConexao conexao;
+
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        conexao = new BleInteradorAndroid(status, _scanTime,nomeDispositivo,_servico,_caracteristica,Receber, botaoConectar,botaoDesconectar,null,null);
+#endif
+#if UNITY_EDITOR
+        conexao = new BleInteradorWin(status, _scanTime, nomeDispositivo, _servico, _caracteristica,Receber, botaoConectar, botaoDesconectar, null, null);
+#endif
+        botaoConectar.interactable = true;
+        botaoDesconectar.interactable = false;
+    }
+#if UNITY_EDITOR
+    private void OnApplicationQuit()
+    {
+        BleApi.Quit();
+    }
+#endif
+
+
+    public void ScanForDevices()
+    {
+        conexao.OnScan();
+    }
+
+    public void DisconnectDevice()
+    {
+        conexao.OnDesconectar();//desconecta o device     
+    }
+
+    private void Update()
+    {
+        conexao.Update_Unity();
+    }
+
+    private void OnDisconnected(string deviceUuid)
+    {
+        _connectCommand.End();
+    }
+
+    public void EnviarOn()
+    {
+        conexao.Enviar("on\n");
+    }
+    public void EnviarOff()
+    {
+        conexao.Enviar("off\n");
+    }
+
+    public void EnviarJogos()
+    {
+        if (Input.GetButtonDown("NomeDoBotao")) //iniciar jogo
+        {
+            conexao.Enviar("NUMERO");
+        }
+        if (Input.GetButtonDown("NomeDoBotao")) //Resposta certa
+        {
+            conexao.Enviar("C");
+        }
+        if (Input.GetButtonDown("NomeDoBotao")) //Resposta errada
+        {
+            conexao.Enviar("E");
+        }
+    }
+    public void Receber(string dados)
+    {
+        string receber = Console.ReadLine();
+        status.text = dados;
+
+        if (dados == "MUSICA") //Tocar a musica
+        {
+            conexao.Enviar("JOGOMUSIC1");
+        }
+        if (dados == "T") //Chamar tela que acertou e envia pro console FIM 
+        {
+
+            Debug.Log("FIM");
+        }
+        if (dados == "F") //Chamar tela que errou e envia pro console FIM 
+        {
+
+            Debug.Log("FIM");
+        }
+        if (receber == "FIM") { //Volta para a tela de atividades
+            conexao.Enviar("ACABOU");
+
+        }
+    }
+}
