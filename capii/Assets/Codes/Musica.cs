59e60c7c9195d93ab290d651c51f5c663a4cbd4b
capiproject <165906396+capiproject@users.noreply.github.com>
2024-04-16 12:20:33 -0300

    musica

---
+++ capii/Assets/Codes/Musica.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes7 : MonoBehaviour
+{
+    public void GoToMusica()
+    {
+        SceneManager.LoadScene("Musicas");
+    }
+}
