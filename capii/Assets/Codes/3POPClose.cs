b888f9972416babcd6c0b81a9e714fb11971b80a
capiproject <165906396+capiproject@users.noreply.github.com>
2024-04-17 12:04:38 -0300

    kkk

---
+++ capii/Assets/Codes/3POPClose.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes11 : MonoBehaviour
+{
+    public void GoTo3popclose()
+    {
+        SceneManager.LoadScene("Numeros5");
+    }
+}
---
+++ capii/Assets/Codes/3to4.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes12 : MonoBehaviour
+{
+    public void GoTo4()
+    {
+        SceneManager.LoadScene("Numeros8");
+    }
+}
